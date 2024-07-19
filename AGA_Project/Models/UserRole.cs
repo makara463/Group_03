b8c79f556d9424ec60f0e0c94e6459b954d6a186
panda <144907665+makara463@users.noreply.github.com>
2024-07-19 20:02:07 +0700

    userrole update allready

--- AGA_Project/Models/UserRole.cs
+++ AGA_Project/Models/UserRole.cs
@@ -2,5 +2,11 @@
 {
     public class UserRole
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string RoleId { get; set; }
+        = Guid.NewGuid().ToString();
+            public string RoleName { get; set; } = Guid.NewGuid().ToString();
     }
 }
