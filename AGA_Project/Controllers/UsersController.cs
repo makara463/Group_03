f0b6e7e4e9b8ccd995dd6abec6ec7d87024f777b
panda <144907665+makara463@users.noreply.github.com>
2024-09-06 08:39:55 +0700

    UsersController has been created

---
+++ AGA_Project/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AGA_Project.Controllers
+{
+    public class UsersController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
