39f2c239699c49af101dae148204f3f5b08ff8b0
panda <144907665+makara463@users.noreply.github.com>
2024-09-16 10:17:30 +0700

    Crud operation about role was implement in Asp .net mvc framework

---
+++ Testing_data_annotation/Controllers/EmployeeContoller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testing_data_annotation.Models;
+
+namespace Testing_data_annotation.Controllers
+{
+    public class EmployeeContoller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Testing_data_annotation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Testing_data_annotation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
