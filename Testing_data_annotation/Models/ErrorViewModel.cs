c338ed931e6d6a8618412f92ccdac9a253ef9fdd
makara463 <144907665+makara463@users.noreply.github.com>
2024-09-17 07:50:12 +0700

    Delete Testing_data_annotation directory

--- Testing_data_annotation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Testing_data_annotation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
