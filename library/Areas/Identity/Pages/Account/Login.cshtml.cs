f9525f4d94e7a92a00d03eb27f7fef01bfc6397b
panda <144907665+makara463@users.noreply.github.com>
2024-10-09 14:40:55 +0700

    library dashboard was created

--- library/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ library/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -91,7 +91,7 @@ namespace library.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
 
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Admin/Index");
 
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
@@ -103,7 +103,7 @@ namespace library.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Admin/Index");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
